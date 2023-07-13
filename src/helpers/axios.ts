674ad6612a9822c1ddb2676b50b95155db558f6b
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-07-13 04:01:54 -0400

    chore: 🔧 adding interceptor

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -4,4 +4,21 @@ const api = axios.create({
 	baseURL: import.meta.env.VITE_BASE_URL
 })
 
+api.interceptors.request.use(
+	function (config) {
+		config.params = {
+			...config.params,
+			from: '2023-07-11',
+			to: '2023-07-11',
+			sortBy: 'popularity',
+			language: 'pt',
+			apiKey: import.meta.env.VITE_API_KEY
+		}
+		return config
+	},
+	function (err) {
+		return Promise.reject(err)
+	}
+)
+
 export default api
